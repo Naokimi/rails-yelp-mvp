a2a2c8e831952839553c27cfe99d2ea2cdfb6dd5
Paulo DAlberti <phdalberti@gmail.com>
2019-05-22 17:17:42 +0900

    working copycat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Deleting restaurants..."
+Restaurant.destroy_all
+puts "Restaurants deleted."
+puts "Creating restaurants..."
+RESTAURANTS = [
+  {
+    name:     "Restaurace Pulcinella",
+    address:  "Plaská 64, Plzeň",
+    category: "italian"
+  },
+  {
+    name:     "Zeke - Sushi bar",
+    address:  "Smetanovy sady 2/13, Plzeň",
+    category: "japanese"
+  },
+  {
+    name:     "Zlatý drak",
+    address:  "Americká 16/26, Plzeň",
+    category: "chinese"
+  },
+  {
+    name:     "Pizzeria Da Pietro",
+    address:  "Smetanovy sady 331/4, Plzeň",
+    category: "italian"
+  },
+  {
+    name:     "Le Frenchie café",
+    address:  "Smetanovy sady 332/6, Plzeň",
+    category: "french"
+  }
+]
+Restaurant.create!(RESTAURANTS)
+puts "Restaurants created."
